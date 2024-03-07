8f7dde2372743a247805c6e866a348b9203522ba
jonahh2160 <144245615+jonahh2160@users.noreply.github.com>
2024-03-07 14:57:06 -0600

    Slot class implementation

--- src/Slot.java
+++ src/Slot.java
@@ -1,3 +1,43 @@
 public class Slot {
+    private byte x;
+    private byte y;
+    private byte color = -1;
+    private boolean filled = false;
 
+    public Slot(byte x, byte y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public byte getX() {
+        return x;
+    }
+
+    public void setX(byte x) {
+        this.x = x;
+    }
+
+    public byte getY() {
+        return y;
+    }
+
+    public void setY(byte y) {
+        this.y = y;
+    }
+
+    public byte getColor() {
+        return color;
+    }
+
+    public void setColor(byte color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
 }
