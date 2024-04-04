9aaeb21e03e47ae1b7bc96cd1c5e2200e1ed4f92
Michael Toon <michael.toon@united-systems.com>
2024-04-04 11:43:58 -0500

    Cleaning up code

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -16,10 +16,6 @@ public class MainScreen implements ActionListener {
         frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("WELCOME TO BLOCKUS!");
-        // frame.add(panel, BorderLayout.CENTER);
-
-        // JPanel panel = new JPanel();
-        // panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
@@ -29,13 +25,7 @@ public class MainScreen implements ActionListener {
         frame.add(panel);
         frame.setLocationRelativeTo(null);
 
-        // frame.pack();
-
-        // panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 80, 100));
-        // panel.setLayout(new GridLayout(2, 0));
-
         JButton singlePlayerButton = new JButton("Start Single Player Game!");
-        // singlePlayerButton.setBounds(10, 20, 80, 25);
         singlePlayerButton.setPreferredSize(new Dimension(300, 60));
         singlePlayerButton.setBackground(Color.GREEN);
         singlePlayerButton.addActionListener(this);
@@ -43,7 +33,6 @@ public class MainScreen implements ActionListener {
         panel.add(Box.createVerticalStrut(80));
 
         JButton multiPlayerButton = new JButton(" Start Multi-player Game! ");
-        // multiPlayerButton.setBounds(10, 40, 80, 25);
         multiPlayerButton.setPreferredSize(new Dimension(300, 60));
         multiPlayerButton.setBackground(Color.RED);
         multiPlayerButton.addActionListener(this);
