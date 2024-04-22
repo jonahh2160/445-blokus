13aa4a1b0f4bc6f403640ccb2f4ade834505876c
Michael Toon <michael.toon@united-systems.com>
2024-04-21 23:27:50 -0500

    Incorporating Jonah's findMove() into GameFlow and setting variables to keep track of end game state based on what it returns

--- src/GameFlow.java
+++ src/GameFlow.java
@@ -1,10 +1,12 @@
 // MT 
+
 public class GameFlow {
     private GUI clicked;
     private GUI blokusGUI;
     private GameBoard gameBoard;
     private GameLogic logic;
     private Boolean gameOver = false;
+    private Boolean redHasNoMoves, blueHasNoMoves, greenHasNoMoves, yellowHasNoMoves = false;
     private PieceColor blue = PieceColor.BLUE;
     private PieceColor yellow = PieceColor.YELLOW;
     private PieceColor red = PieceColor.RED;
@@ -32,20 +34,20 @@ public class GameFlow {
 
         for(int i = 0; i == 1; i++){
             logic.validFirstMove(piece, clicked.getX(), clicked.getY());
-            playerTurn(red, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(red, invRed, piece, gameBoard, clicked.getX(), clicked.getY());
             logic.validFirstMove(piece, clicked.getX(), clicked.getY());
-            playerTurn(blue, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(blue, invBlue, piece, gameBoard, clicked.getX(), clicked.getY());
             logic.validFirstMove(piece, clicked.getX(), clicked.getY());
-            playerTurn(green, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(green, invGreen, piece, gameBoard, clicked.getX(), clicked.getY());
             logic.validFirstMove(piece, clicked.getX(), clicked.getY());
-            playerTurn(yellow, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(yellow, invYellow, piece, gameBoard, clicked.getX(), clicked.getY());
         }
 
         while (!gameOver) {
-            playerTurn(red, piece, gameBoard, clicked.getX(), clicked.getY());
-            playerTurn(blue, piece, gameBoard, clicked.getX(), clicked.getY());
-            playerTurn(green, piece, gameBoard, clicked.getX(), clicked.getY());
-            playerTurn(yellow, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(red, invRed, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(blue, invBlue, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(green, invGreen, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(yellow, invYellow, piece, gameBoard, clicked.getX(), clicked.getY());
             endGameCheck();
         }
 
@@ -64,40 +66,55 @@ public class GameFlow {
 
         for(int i = 0; i == 1; i++){
             logic.validFirstMove(piece, clicked.getX(), clicked.getY());
-            playerTurn(red, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(red, invRed, piece, gameBoard, clicked.getX(), clicked.getY());
             logic.validFirstMove(piece, clicked.getX(), clicked.getY());
-            playerTurn(blue, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(blue, invBlue, piece, gameBoard, clicked.getX(), clicked.getY());
             logic.validFirstMove(piece, clicked.getX(), clicked.getY());
-            playerTurn(green, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(green, invGreen, piece, gameBoard, clicked.getX(), clicked.getY());
             logic.validFirstMove(piece, clicked.getX(), clicked.getY());
-            playerTurn(yellow, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(yellow, invYellow, piece, gameBoard, clicked.getX(), clicked.getY());
         }
 
         while (!gameOver) {
 
-            playerTurn(red, piece, gameBoard, clicked.getX(), clicked.getY());
-            playerTurn(blue, piece, gameBoard, clicked.getX(), clicked.getY());
-            playerTurn(green, piece, gameBoard, clicked.getX(), clicked.getY());
-            playerTurn(yellow, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(red, invRed, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(blue, invBlue, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(green, invGreen, piece, gameBoard, clicked.getX(), clicked.getY());
+            playerTurn(yellow, invYellow, piece, gameBoard, clicked.getX(), clicked.getY());
             endGameCheck();
 
         }
     }
 
     // MT: trying to work through logic of player turn
-    void playerTurn(PieceColor color, Piece piece, GameBoard gameBoard, int xCoordinate, int yCoordinate) {
+    void playerTurn(PieceColor color, Piece[] pieceInventory, Piece piece, GameBoard gameBoard, int xCoordinate, int yCoordinate) {
+         
         //Here we'll place Jonah's method to check for available moves first
-        //Selects piece to play
-        logic.pieceSelect(piece);
-        //Places piece on board
-        gameBoard.placePiece(piece, xCoordinate, yCoordinate);
-        //Method to keep track of last piece for each color
-        setLastPieceAs(color, piece);
-
+        Move availableMove = logic.findMove(invBlue, gameBoard);
+        if(availableMove == null){
+            setHasNoMoves(color);
+            System.out.println("Sorry you have no available moves! Skipping your turn....");
+        }else{
+            //Selects piece to play
+            logic.pieceSelect(piece);
+            //Checks if piece selected is legal
+            boolean pieceIsLegal = gameBoard.isPieceLegal(piece, xCoordinate, yCoordinate);
+            //IF statement to execute logic based on if piece is legal 
+            if(!pieceIsLegal){
+                System.out.println("This move is not legal. Please try again!");
+            }else{
+                //Places piece on board
+                gameBoard.placePiece(piece, xCoordinate, yCoordinate);
+                //Method to keep track of last piece for each color
+                setLastPieceAs(color, piece);
+                }
+        }
+        
     }
 
     //MT method to update the last piece placed for each color
     void setLastPieceAs(PieceColor color, Piece piece){
+        
         if(color == red){
             lastPieceRed = piece;
         } else if(color == blue){
@@ -109,14 +126,26 @@ public class GameFlow {
         }
     }
 
+    //MT method to keep track of no moves for each color for end game check
+    void setHasNoMoves(PieceColor color){
+        
+        if(color == red){
+            redHasNoMoves = true;
+        }else if(color == blue){
+            blueHasNoMoves = true;
+        }else if (color == green){
+            greenHasNoMoves = true;
+        }else{
+            yellowHasNoMoves = true;
+        }
+    }
+
     //Mt Still need to configure for what Jonah's method is returning to make gameOverSignifier equivalent to it
     void endGameCheck(){
-        //MT here we'll check whatever Jonah's method return and depending on it returns execute logic
-        String gameOverSignifier = " ";
-        //gameOverSignifier = logic.findMove();
-        if( gameOverSignifier == null){
-            endGame();
-        }
+
+       if(redHasNoMoves == true && blueHasNoMoves == true && greenHasNoMoves == true && yellowHasNoMoves == true){
+        endGame();
+       }
 
     }
 
